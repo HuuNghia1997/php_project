a47369c898ae1214cbfb400f4172d155706331d8
Trankadzo <trankadzo12345@gmail.com>
2020-12-05 20:06:41 +0700

    code

--- read_custommer.php
+++ read_custommer.php
@@ -4,10 +4,8 @@ header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-
 include_once './connection.php';
 include_once './custommer.php';
-
 $database = new Database();
 $db = $database->getConnection();
 $item = new Custommer($db);
